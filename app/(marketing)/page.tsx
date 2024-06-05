bae8834fb230fb44f47562513ab3f47672ca3c79
Ania Kowalska <a.kowalska622@gmail.com>
2024-06-05 11:00:17 +0200

    add header and footer to the layout

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div>
-      <Button size='lg' variant='destructive'>
+      <Button size='lg' variant='primary'>
         click me
       </Button>
     </div>
